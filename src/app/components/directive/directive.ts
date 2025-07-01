24852713c8ccd2c1cd806b3d0ddf2a897587f085
Nikhil Singhal <nikhilsinghal000@gmail..com>
2025-07-01 18:01:31 +0530

    attribute directive

--- src/app/components/directive/directive.ts
+++ src/app/components/directive/directive.ts
@@ -1,15 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-directive',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './directive.html',
   styleUrl: './directive.scss',
 })
 export class Directive {
   gender = 'male';
   hobbies = ['cricket', 'coding', 'reading'];
-  color="red11"
+  color = 'red11';
+
+  isDanger = true;
 }
 
 // he/she is a painter
