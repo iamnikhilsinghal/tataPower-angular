324f0b6db5c5eab57b2297704a9a53eb5b085a69
Nikhil Singhal <nikhilsinghal000@gmail..com>
2025-07-02 17:28:32 +0530

    user events custom directive

--- src/app/directives/highlight.ts
+++ src/app/directives/highlight.ts
@@ -1,15 +1,30 @@
-import { Directive, ElementRef, inject } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  inject,
+  input,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
 export class Highlight {
-
-  
   private el = inject(ElementRef);
 
-  constructor() {
-    // document.getElementById('id')?.style.backgroundColor
-    this.el.nativeElement.style.backgroundColor = 'yellow';
+  // constructor() {
+  //   // document.getElementById('id')?.style.backgroundColor
+  //   this.el.nativeElement.style.backgroundColor = 'red';
+  // }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
   }
 }
