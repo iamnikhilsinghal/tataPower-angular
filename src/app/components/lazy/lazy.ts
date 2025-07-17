73280b08bbd40e28ec458070798b82d6c138e8ef
Nikhil Singhal <nikhilsinghal000@gmail..com>
2025-07-17 16:34:22 +0530

    signals

--- src/app/components/lazy/lazy.ts
+++ src/app/components/lazy/lazy.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-lazy',
   imports: [],
   templateUrl: './lazy.html',
-  styleUrl: './lazy.scss'
+  styleUrl: './lazy.scss',
 })
 export class Lazy {
+  // const mySignal = signal(initialValue);
 
+  // x = 5;
+  // y = 10;
+  // sum = 0;
+
+  // ngOnInit() {
+  //   this.sum = this.x + this.y;
+  //   console.log('this.sum', this.sum);
+  //   this.y = 8;
+  //   console.log('this.sum 2', this.sum);
+  // }
+
+  constructor() {
+    const count = signal(0);
+    const count2 = signal(0);
+
+    effect(() => {
+      console.log(`Count changed to: ${count()} ${count2()}`);
+      // search query
+      // task signal(variable) depend
+    });
+
+    count.set(1);
+    count.set(2);
+    count.set(3);
+    count2.set(16);
+  }
+
+  ngOnInit() {
+    // const x = signal(2);
+    // const y = signal(3);
+    // const sum = computed(() => x() + y());
+    // console.log('sum 1', sum()); // Outputs: 5
+    // // Update one of the signals
+    // x.set(5);
+    // console.log('sum 2', sum()); //8
+  }
 }
