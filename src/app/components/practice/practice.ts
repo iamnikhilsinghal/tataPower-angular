e2dd13cf3dff70f17a0b4a1bb69d8a3b235c1f8f
Nikhil Singhal <nikhilsinghal000@gmail..com>
2025-07-18 17:56:16 +0530

    day 21- imp links for project

---
+++ src/app/components/practice/practice.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practice',
+  imports: [],
+  templateUrl: './practice.html',
+  styleUrl: './practice.scss',
+})
+export class Practice {}
